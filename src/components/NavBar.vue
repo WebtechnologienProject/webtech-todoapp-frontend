eb185d2f927024d931702c9ccf5b7acabaee4501
WebtechnologienProject <104024744+WebtechnologienProject@users.noreply.github.com>
2022-06-12 17:48:01 +0200

    Corrected link to heroku-backup

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">ToDo List</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <a class="navbar-brand">ToDo List</a>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/about">About</router-link>
+          <router-link class="nav-link" to="/">My day </router-link>
           <router-link class="nav-link" to="/todo">ToDo</router-link>
+          <router-link class="nav-link" to="/about">About</router-link>
         </div>
       </div>
     </div>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,82 +1,57 @@
 <template>
   <h1>Welcome to ToDo-List</h1>
-  <div class="page-content page-container" style="align-content: center">
-    <div class="align-content-center">
-      <input
-        type="text"
-        v-model=todos.title
-        placeholder="Enter new task"
-        class="w-100 from control"
-      />
-      <input
-        type="text"
-        v-model=todos.description
-        placeholder="Enter description"
-        class="w-100 from control"
-      />
-      <select class="form-select" aria-label="Default select example">
-        <option selected>My Day</option>
-        <option value="1">Important </option>
-        <option value="2">Not Important</option>
-        <option value="3">Burning</option>
-        <option value="4">Not burning</option>
-      </select>
-      <button class="btn btn-warning rounded-0" @click="submitTask">
-        SUBMIT
-      </button>
-    </div>
-    <table class="table table-bordered mt-5">
-      <thead>
-      <tr>
-        <th scope="col">Task</th>
-        <th scope="col" style="width: 120px">Description</th>
-        <th scope="col" class="text-center">Category</th>
-        <th scope="col" class="text-center">###</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="todo in todos" :key="todo.todoId">
-        <td>
-            <span>
-              {{ todo.title }}
-            </span>
-        </td>
-        <td>
-            <span>
-              {{ todo.description }}
-            </span>
-        </td>
-        <td>
-            <span>
-              {{ todo.category }}
-            </span>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+  <todo-create-form :categories="this.categories"></todo-create-form>
+  <div id="todo-list">
+    <todo-table :todos="this.todos"></todo-table>
   </div>
 </template>
 
 <script>
+import TodoTable from '@/components/TodoTable'
+import TodoCreateForm from '@/components/TodoCreateForm'
 export default {
   name: 'TodoView',
+  components: { TodoCreateForm, TodoTable },
   data () {
     return {
-      todos: []
+      todos: [],
+      categories: []
     }
   },
-  mounted () {
-    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/todo'
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+  methods: {
+    loadTodos () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/todo'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => result.forEach(todo => {
+          this.todos.push(todo)
+        }))
+        .catch(error => console.log('error', error))
+    },
+    loadCategories () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/categories'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => result.forEach(category => {
+          this.categories.push(category)
+        }))
+        .catch(error => console.log('error', error))
     }
-    fetch(endpoint, requestOptions)
-      .then(response => response.json())
-      .then(result => result.forEach(todo => {
-        this.todos.push(todo)
-      }))
-      .catch(error => console.log('error', error))
+  },
+  async created () {
+    this.loadTodos()
+    this.loadCategories()
+  },
+  mounted () {
   }
 }
 </script>

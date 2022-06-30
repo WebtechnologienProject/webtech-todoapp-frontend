3280c6c81e7dfd3eb7b3aeace3b67c9319d2b183
WebtechnologienProject <104024744+WebtechnologienProject@users.noreply.github.com>
2022-06-30 22:56:27 +0200

    Created TodoTabel and NewTodoForm

---
+++ src/components/TodoCreateForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <button class="btn btn-primary static-button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">
+    Add ➕
+  </button>
+  <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasBottomLabel">Adding new todo</h5>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <form class="row row-cols-lg-auto g-3 align-items-center">
+        <div class="col-12">
+          <label for="title" class="visually-hidden">Title</label>
+          <div class="input-group">
+            <input type="text" class="form-control"  placeholder="Enter your title" id="title" v-model="title" required>
+            <div class="invalid-feedback">
+              Please enter a title of todo.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <label for="description" class="visually-hidden">Description</label>
+          <div class="input-group">
+            <input type="text" class="form-control"  placeholder="Enter your description" id="description" v-model="description" required>
+          </div>
+        </div>
+
+<!--        <div class="col-12">-->
+<!--          <label class="form-label" for="category">Category</label>-->
+<!--          <div class="input-group">-->
+<!--            <input type="range" class="form-control" id="category" placeholder="Category" v-model="category.categoryTitle">-->
+<!--            <select id="category" name="category" form="category">-->
+<!--              <option v-bind="category in categories" :key="category.categoryId"> {{ category.categoryTitle }} </option>-->
+<!--              </select>-->
+<!--          </div>-->
+          <div class="col-12">
+            <label for="category" class="form-label">Category</label>
+            <select id="selectedCategory" class="form-select" v-model="selectedCategory.categoryId">
+              <option value="" selected disabled> Choose...</option>
+              <option v-for="cat in categories" :key="cat.categoryId" v-bind:value="cat.categoryId"> {{ cat.categoryTitle }} </option>
+            </select>
+            <div class="invalid-feedback">
+              Please select a valid category.
+            </div>
+        </div>
+
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+          <button type="button" class="btn btn-success" @click="createTodo">Submit</button>
+          <button type="button" class="btn btn-danger">Reset</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoCreateForm',
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      title: '',
+      description: '',
+      selectedCategory: [{
+        categoryId: '',
+        categoryTitle: ''
+      }]
+    }
+  },
+  emits: ['created'],
+  methods: {
+    createTodo () {
+      // const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
+      // const endpoint = baseUrl + '/api/todoo
+      const endpoint = 'http://localhost:8080/api/todo'
+      console.log('New todo Created')
+      const myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+      const raw = JSON.stringify({
+        title: this.title,
+        description: this.description,
+        category: {
+          categoryId: this.selectedCategory.categoryId,
+          categoryTitle: this.selectedCategory.categoryTitle
+        }
+      })
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log('Success:', result))
+        .catch(error => console.log('error', error))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.static-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 15px;
+  border-radius: 30px;
+}
+</style>
---
+++ src/components/TodoTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <table class="table table-bordered mt-5">
+    <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Category</th>
+      <th scope="col">Edit</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-if="todos.length === 0">
+      <td colspan="6">You haven't saved any todos yet</td>
+    </tr>
+    <tr class="col" v-for="todo in todos" :key="todo.todoId">
+      <th scope="row">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="done" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+        </label>
+      </div>
+      </th>
+      <td> {{ todo.title }} </td>
+      <td> {{ todo.description }} </td>
+      <td> {{ todo.category.categoryTitle }} </td>
+      <td>
+        <button style="margin-left: 10px" @click="editTask">✏️</button>
+        <button style="margin-left: 10px" @click="toMyDay"> 🌞 </button>
+        <button style="margin-left: 10px" @click="deleteTask">❌ </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TodoTable',
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      title: this.title,
+      description: this.description,
+      category: {
+        categoryId: this.categoryId,
+        categoryTitle: this.categoryTitle
+      },
+      done: false
+    }
+  },
+  methods: {
+    editTask () {
+    },
+    deleteTask () {
+    },
+    toMyDay () {
+    },
+    doneTask () {
+      this.done = []
+      for (const i in this.todos) {
+        this.done.push(this.todos[i].todoId)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
